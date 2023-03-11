590a1ecafbfeaceb2e3aa4b2b4cae997261fd36f
ssaditya2002 <ssaditya2002@gmail.com>
2023-03-12 04:32:07 +0530

    Deployed

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const userRoute = require("./routes/userRoute");
 const path = require("path");
 app.use("/api/user/", userRoute);
 
+const path = require("path");
+__dirname = path.resolve();
+// render deployment
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
